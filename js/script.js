57b9e7116ade4d174d31c03c9ff1fc5e4238a53f
Andrei Trifu <Andrei.Trifu84@gmail.com>
2023-10-17 12:40:39 +0100

    Contact me modal added

--- js/script.js
+++ js/script.js
@@ -1,6 +1,9 @@
 let onButton = document.querySelector(".modal-on");
 let modal = document.querySelector(".about-me-modal");
+let contactModal = document.querySelector(".contact-me-modal");
 let offButton = document.querySelector(".modal-off");
+let contactmebuttonOn = document.querySelector(".contact-me-button");
+let contactmebuttonOff = document.querySelector(".contact-modal-off");
 
 onButton.addEventListener("click", () => {
   modal.style.display = "block";
@@ -8,4 +11,12 @@ onButton.addEventListener("click", () => {
 
 offButton.addEventListener("click", () => {
   modal.style.display = "none";
+});
+
+contactmebuttonOn.addEventListener("click", () => {
+  contactModal.style.display = "block";
+});
+
+contactmebuttonOff.addEventListener("click", () => {
+  contactModal.style.display = "none";
 });
