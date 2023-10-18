1f2f65319d9bef70f77653f522b1ffae0540b14d
Andrei Trifu <Andrei.Trifu84@gmail.com>
2023-10-18 18:32:42 +0100

    Portfolio page styled

---
+++ js/portfolio.js
@@ -0,0 +1,32 @@
+const displayTarget = document.querySelector(".display");
+
+fetch("./js/portfolio.json")
+  .then((response) => {
+    return response.json();
+  })
+  .then((data) => {
+    let output = "";
+
+    data.forEach((project) => {
+      output += `
+      <div class='w-full relative'>
+        <div class='h-[320px] w-full card-container'>
+            <img class="rounded-[20px] w-full h-full" alt="'A webpage with one row and three columns " src="${project.img}"/>
+        </div> 
+        <div class='w-[80%] p-2 rounded-[20px] card-text'>
+          <div class='mt-2 mb-3 flex gap-1 w-6'>
+            <img src=${project.icons[0]} />
+            <img src=${project.icons[1]} />
+            <img src=${project.icons[2]} />
+          </div>
+          <div>
+            <p class='text-xl'>${project.title}<\p>
+            <p class='text-gray-400 card-project-description'>${project.description}</p>
+          </div> 
+        </div>
+      </div>`;
+
+      displayTarget.innerHTML = output;
+      console.log(project);
+    });
+  });
